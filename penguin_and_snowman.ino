87ca69b8a42b271ef485eb249c093e78a2bd8ccb
AnotherMatt92 <108900488+AnotherMatt92@users.noreply.github.com>
2023-11-17 19:27:51 +0000

    added umbrella rainbow fade
    added umbrella rainbow fade. Bug prevents use of Pi Pico so Teensy 3.6 is what I have tested it with sucessfully

--- penguin_and_snowman.ino
+++ penguin_and_snowman.ino
@@ -6,7 +6,7 @@
 #define BRIGHTNESS 96
 
 #define LED_TYPE WS2811
-#define COLOR_ORDER GRB
+#define COLOR_ORDER RGB
 
 CRGB body[BODY_END - BODY_START];
 CRGB head[HEAD_END - HEAD_START];
@@ -15,7 +15,10 @@ CRGB s_umbrella[S_UMBRELLA_END - S_UMBRELLA_START];
 CRGB p_umbrella[P_UMBRELLA_END - P_UMBRELLA_START];
 CRGB penguins[PENGUINS_END - PENGUINS_START];
 
+uint8_t global_hue = 0;
+
 void setup() {
+    delay(3000);
     FastLED.addLeds<LED_TYPE, DATA_PIN_BODY,COLOR_ORDER>
     (body, BODY_END - BODY_START).setCorrection(TypicalLEDStrip);
     FastLED.addLeds<LED_TYPE, DATA_PIN_HEAD,COLOR_ORDER>
@@ -36,7 +39,7 @@ void loop() {
   // Snowman Body
   color_item(body, BODY_START, BODY_END, BODY_COLOR);
   
-  // Snowman Head
+  // // Snowman Head
   color_item(head, HAT_START, HAT_END, HAT_COLOR);
   color_item(head, FACE_START, FACE_END, FACE_COLOR);
   color_item(head, MOUTH_START, MOUTH_END, MOUTH_COLOR);
@@ -58,13 +61,134 @@ void loop() {
   color_item(p_umbrella, P_UMBRELLA_START, P_UMBRELLA_END, P_UMBRELLA_COLOR);
 
   // Penguin 1 body
-  color_item(penguins, P_1_BODY_START, 
+  // color_item(penguins, P_1_BODY_START, 
+
+  p_umbrella_rainbow_wipe(global_hue);
+  s_umbrella_rainbow_wipe(global_hue+80);
+  global_hue--;
+  delay(100);
 
   FastLED.show();
 }
 
 void color_item(CRGB section[], int start_led, int end_led, int color) {
-   for( int i = start_led; i <= end_led; i++) {
+   for( int i = start_led; i < end_led; i++) {
        section[i] = color;
    }    
+}
+
+//Penguin umbrella colour wipe
+void p_umbrella_rainbow_wipe(uint8_t hue){
+  uint8_t spread = 120; //amount of hue across the umbrella
+  uint8_t spread_mid = spread/2; //halfway point
+  CHSV hsv;
+  hsv.hue = hue;
+  hsv.val = 255;
+  hsv.sat = 240;
+  //go thought section by section in order as they are wired up
+  //left center rib
+  for (uint8_t i = 0; i <P_UMBRELLA_HANDLE_CNT ; i++ ){
+    hsv.hue = hue + spread_mid; //last 2 digits map position from left to right of the rib
+    p_umbrella[i + P_UMBRELLA_HANDLE_START] = hsv;
+  }
+
+
+  //left center rib
+  for (uint8_t i = 0; i < P_UMBRELLA_L_RIB_CENTRE_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_L_RIB_CENTRE_CNT,spread*0.33, spread_mid); //last 2 digits map position from left to right of the rib
+    p_umbrella[i + P_UMBRELLA_L_RIB_CENTRE_START] = hsv;
+  }
+
+  //right mid rib
+  for (uint8_t i = 0; i < P_UMBRELLA_R_RIB_MID_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_R_RIB_MID_CNT,spread_mid, spread*0.8); //last 2 digits map position from left to right of the rib
+    p_umbrella[i+ P_UMBRELLA_R_RIB_MID_START] = hsv;
+  }
+  
+  //right center rib
+  for (uint8_t i = 0; i < P_UMBRELLA_R_RIB_CENTRE_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_R_RIB_CENTRE_CNT,spread*.67, spread_mid); //last 2 digits map position from left to right of the rib
+    p_umbrella[i+ P_UMBRELLA_R_RIB_CENTRE_START] = hsv;
+  }
+
+  //left mid rib
+  for (uint8_t i = 0; i < P_UMBRELLA_L_RIB_MID_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_L_RIB_MID_CNT,spread_mid, spread*0.2); //last 2 digits map position from left to right of the rib
+    p_umbrella[i+ P_UMBRELLA_L_RIB_MID_START] = hsv;
+  }  
+
+  //left outer rib
+  for (uint8_t i = 0; i < P_UMBRELLA_L_RIB_OUTER_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_L_RIB_OUTER_CNT,0, spread_mid); //last 2 digits map position from left to right of the rib
+    p_umbrella[i+ P_UMBRELLA_L_RIB_OUTER_START] = hsv;
+  }    
+  //right outer rib
+  for (uint8_t i = 0; i < P_UMBRELLA_R_RIB_OUTER_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_R_RIB_OUTER_CNT,spread_mid, spread); //last 2 digits map position from left to right of the rib
+    p_umbrella[i+ P_UMBRELLA_R_RIB_OUTER_START] = hsv;
+  }    
+  
+  //right outer rib
+  for (uint8_t i = 0; i < P_UMBRELLA_BTM_CANOPY_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, P_UMBRELLA_BTM_CANOPY_CNT,spread, 0); //last 2 digits map position from left to right of the rib
+    p_umbrella[i+ P_UMBRELLA_BTM_CANOPY_START] = hsv;
+  } 
+}
+
+//Snowman umbrella colour wipe
+void s_umbrella_rainbow_wipe(uint8_t hue){
+  uint8_t spread = 160; //amount of hue across the umbrella
+  uint8_t spread_mid = spread/2; //halfway point
+  CHSV hsv;
+  hsv.hue = hue;
+  hsv.val = 255;
+  hsv.sat = 240;
+  //go thought section by section in order as they are wired up
+  //left center rib
+  for (uint8_t i = 0; i <S_UMBRELLA_HANDLE_CNT ; i++ ){
+    hsv.hue = hue + spread_mid; //last 2 digits map position from left to right of the rib
+    s_umbrella[i + S_UMBRELLA_HANDLE_START] = hsv;
+  }
+
+
+  //left center rib
+  for (uint8_t i = 0; i < S_UMBRELLA_L_RIB_CENTRE_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_L_RIB_CENTRE_CNT,spread*0.33, spread_mid); //last 2 digits map position from left to right of the rib
+    s_umbrella[i + S_UMBRELLA_L_RIB_CENTRE_START] = hsv;
+  }
+
+  //right mid rib
+  for (uint8_t i = 0; i < S_UMBRELLA_R_RIB_MID_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_R_RIB_MID_CNT,spread_mid, spread*0.8); //last 2 digits map position from left to right of the rib
+    s_umbrella[i+ S_UMBRELLA_R_RIB_MID_START] = hsv;
+  }
+  
+  //right center rib
+  for (uint8_t i = 0; i < S_UMBRELLA_R_RIB_CENTRE_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_R_RIB_CENTRE_CNT,spread*.67, spread_mid); //last 2 digits map position from left to right of the rib
+    s_umbrella[i+ S_UMBRELLA_R_RIB_CENTRE_START] = hsv;
+  }
+
+  //left mid rib
+  for (uint8_t i = 0; i < S_UMBRELLA_L_RIB_MID_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_L_RIB_MID_CNT,spread_mid, spread*0.2); //last 2 digits map position from left to right of the rib
+    s_umbrella[i+ S_UMBRELLA_L_RIB_MID_START] = hsv;
+  }  
+
+  //left outer rib
+  for (uint8_t i = 0; i < S_UMBRELLA_L_RIB_OUTER_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_L_RIB_OUTER_CNT,0, spread_mid); //last 2 digits map position from left to right of the rib
+    s_umbrella[i+ S_UMBRELLA_L_RIB_OUTER_START] = hsv;
+  }    
+  //right outer rib
+  for (uint8_t i = 0; i < S_UMBRELLA_R_RIB_OUTER_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_R_RIB_OUTER_CNT,spread_mid, spread); //last 2 digits map position from left to right of the rib
+    s_umbrella[i+ S_UMBRELLA_R_RIB_OUTER_START] = hsv;
+  }    
+  
+  //right outer rib
+  for (uint8_t i = 0; i < S_UMBRELLA_BTM_CANOPY_CNT; i++ ){
+    hsv.hue = hue + map(i, 0, S_UMBRELLA_BTM_CANOPY_CNT,spread, 0); //last 2 digits map position from left to right of the rib
+    s_umbrella[i+ S_UMBRELLA_BTM_CANOPY_START] = hsv;
+  } 
 }
